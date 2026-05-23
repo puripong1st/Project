2c4e6f8180e95651978bfea9f12597bb3c851c71
prp <44913650+zazoza1234@users.noreply.github.com>
2026-05-23 21:09:40 +0700

    แก้ไข

---
+++ esp32/build/libraries/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+C:\Users\aunkh\OneDrive\Desktop\Project\esp32\build\libraries\WebServer\detail\mimetable.cpp.o: \
+ C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\libraries\WebServer\src\detail\mimetable.cpp \
+ C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\libraries\WebServer\src\detail\mimetable.h \
+ C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32/pgmspace.h
---
+++ esp32/build/libraries/WebServer/middleware/MiddlewareChain.cpp.d
@@ -0,0 +1,5 @@
+C:\Users\aunkh\OneDrive\Desktop\Project\esp32\build\libraries\WebServer\middleware\MiddlewareChain.cpp.o: \
+ C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\libraries\WebServer\src\middleware\MiddlewareChain.cpp \
+ C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\libraries\WebServer\src\middleware\Middleware.h \
+ C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\tools\esp32-arduino-libs\idf-release_v5.5-9bb7aa84-v2\esp32/include/newlib/platform_include/assert.h \
+ C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\tools\esp32-arduino-libs\idf-release_v5.5-9bb7aa84-v2\esp32/qio_qspi/include/sdkconfig.h
