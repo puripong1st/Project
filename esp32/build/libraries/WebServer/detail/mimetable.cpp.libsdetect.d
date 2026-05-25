7c6901b06990a06bd9597dd1386e7bd05c46f8fa
prp <44913650+zazoza1234@users.noreply.github.com>
2026-05-25 14:53:31 +0700

    feat: initialize project with ESP32 sketch and simulation configuration files

--- esp32/build/libraries/WebServer/detail/mimetable.cpp.libsdetect.d
+++
@@ -1,4 +0,0 @@
-mimetable.o: \
- C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\libraries\WebServer\src\detail\mimetable.cpp \
- C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\libraries\WebServer\src\detail\mimetable.h \
- C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32/pgmspace.h
--- esp32/build/libraries/WebServer/middleware/MiddlewareChain.cpp.libsdetect.d
+++
@@ -1,5 +0,0 @@
-MiddlewareChain.o: \
- C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\libraries\WebServer\src\middleware\MiddlewareChain.cpp \
- C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\libraries\WebServer\src\middleware\Middleware.h \
- C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\tools\esp32-arduino-libs\idf-release_v5.5-9bb7aa84-v2\esp32/include/newlib/platform_include/assert.h \
- C:\Users\aunkh\AppData\Local\Arduino15\packages\esp32\tools\esp32-arduino-libs\idf-release_v5.5-9bb7aa84-v2\esp32/qio_qspi/include/sdkconfig.h
